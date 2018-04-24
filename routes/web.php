8d727422157b993adc668ad7b958c320575b84da
Berry <1913549290@qq.com>
2018-04-24 14:34:00 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@
 //     return view('welcome');
 // });
 Route::get('/', 'PagesController@root')->name('root');
+//
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+// Authentication Routes...
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'AuthController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgetPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgetPasswordController@sendResetLinkReset')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showRequest')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
